4915025bd7b94baf5c1ad63d37a9518e95690358
suchanmyung <aud248@naver.com>
2022-06-04 22:00:01 +0900

    feat : vue blog v1

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,10 +1,25 @@
 <template>
+  <button type="button" class="btn btn-outline-success">Success</button>
+
+  <router-link to="/">홈페이지</router-link>
+  <router-link to="/list">리스트페이지</router-link>
+  <router-view :Blog="Blog"></router-view>
+
+
 </template>
+
 
 <script>
 
+import Blog from './assets/blog.js';
+
 export default {
   name: 'App',
+  data(){
+    return {
+      Blog : Blog,
+    }
+  },
   components: {
   }
 }
---
+++ blog/src/components/List.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h5>{{Blog[0].title}}</h5>
+        <p>{{Blog[0].date}}</p>
+    </div>
+    <div>
+        <h5>{{Blog[1].title}}</h5>
+        <p>{{Blog[1].date}}</p>
+    </div>
+    <div>
+        <h5>{{Blog[2].title}}</h5>
+        <p>{{Blog[2].date}}</p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name : 'list',
+
+    props : {
+        Blog : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
